1610e1b92b99ec9219df1a80c3117f7a54dced65
Paulo <paulosouzap7@gmail.com>
2022-04-22 18:09:52 -0300

    correção no serviço httpclient

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { UsuarioService } from './usuario/usuario.service';
-import { Http2ServerResponse } from 'http2';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
