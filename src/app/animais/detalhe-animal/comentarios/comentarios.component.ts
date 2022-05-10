f55cd138b7d753c76a7eb0cd4891e5bc719618c6
Paulo <paulosouzap7@gmail.com>
2022-05-10 18:23:32 -0300

    inclusao do serviço de like e comentario das fotos

---
+++ src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comentarios } from './comentario';
+
+@Component({
+  selector: 'app-comentarios',
+  templateUrl: './comentarios.component.html',
+  styleUrls: ['./comentarios.component.css']
+})
+export class ComentariosComponent implements OnInit {
+  comentarios$!: Observable<Comentarios>
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
